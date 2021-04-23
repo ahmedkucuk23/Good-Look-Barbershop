4dbff4d6870d4a6224e708034d33cd7f267ad205
ahmed kucuk <ahmed.kucuk123@gmail.com>
2021-04-23 16:33:27 +0200

    ahmo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+  get 'contact/new'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # verb          to: "controler#action"
   root              to: 'pages#home'
